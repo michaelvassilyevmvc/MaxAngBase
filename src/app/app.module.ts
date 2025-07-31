4a12a3042492b33908caf58e70e8b28fa0a182a2
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-31 13:01:40 +0500

    TheBasic.Task2 1. Добавлено поле ввода которое обновляет свойство username с помощью двусторонней привязки + 2. Выведено свойство username с помощью интерполяции строк (в абзаце под полем ввода) + 3. Добавлено кнопку которая может быть нажатой только если свойство username НЕ является пустой строкой + 4. При нажатии кнопки, свойство username должно быть сброшено в пустую строку

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {ServerComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
+import { TaskTwoComponent } from './task-two/task-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    TaskTwoComponent
   ],
   imports: [
     BrowserModule,
