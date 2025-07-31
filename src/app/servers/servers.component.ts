201c6af36b8dad841f7abdb8a4f0fe807ad08b71
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-31 12:13:24 +0500

    Ep27-29. Реализована простая привязка данных для событий и свойств

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created.';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created.';
+  }
 }
