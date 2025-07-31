f6ca207a0f2bb4f7568b0008bb631c7f9be2ee98
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-31 14:27:02 +0500

    Ep36-39. Использование *ngIf, ng-template, ng-else

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';
   serverName = 'Test';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -17,6 +18,7 @@ export class ServersComponent {
   }
 
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created. Name is '+this.serverName;
   }
 
